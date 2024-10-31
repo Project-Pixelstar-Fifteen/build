a172741335f49106d890579dadb0f0b3042a05ff
clarencelol <clarencekuiek@proton.me>
2024-10-31 21:34:33 +0800

    fixup! build: Add pixelage build support
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- envsetup.sh
+++ envsetup.sh
@@ -500,6 +500,13 @@ function lunch()
 
 function _lunch_meat()
 {
+    if (echo -n $1 | grep -q -e "^pixelage_") ; then
+        PIXELAGE_BUILD=$(echo -n $1 | sed -e 's/^pixelage_//g')
+    else
+        PIXELAGE_BUILD=
+    fi
+    export PIXELAGE_BUILD
+
     local product=$1
     local release=$2
     local variant=$3
